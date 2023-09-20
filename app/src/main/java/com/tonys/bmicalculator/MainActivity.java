6ceba1fd448e5d4dc8952ccca7295e853ba810f7
Tony Nguyen <tonynguyen@Tonys-MacBook-Air.local>
2023-09-20 11:15:32 -0700

    first commit

---
+++ app/src/main/java/com/tonys/bmicalculator/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tonys.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Pop ups are lame sauce!", Toast.LENGTH_LONG).show();
+    }
+}
