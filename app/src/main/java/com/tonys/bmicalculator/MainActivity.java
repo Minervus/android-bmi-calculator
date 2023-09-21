8fa35cfd29a6affa801cdf6e25a2d41d677c27dc
Minervus <tnguyen411@gmail.com>
2023-09-21 13:51:55 -0700

    Second Commit

--- app/src/main/java/com/tonys/bmicalculator/MainActivity.java
+++ app/src/main/java/com/tonys/bmicalculator/MainActivity.java
@@ -3,7 +3,14 @@ package com.tonys.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Pop ups are lame sauce!", Toast.LENGTH_LONG).show();
+        RadioButton radioMale = findViewById(R.id.radio_button_male);
+        RadioButton radioFemale = findViewById(R.id.radio_button_female);
+
+        EditText ageText = findViewById(R.id.text_view_age);
+        EditText feetText = findViewById(R.id.text_view_feet);
+        EditText inchesText = findViewById(R.id.text_view_inches);
+        EditText weightText = findViewById(R.id.text_view_weight);
+
+        Button calculateButton = findViewById(R.id.button_calculate);
+        TextView resultText = findViewById(R.id.text_view_result);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Autocomplete is OP", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
