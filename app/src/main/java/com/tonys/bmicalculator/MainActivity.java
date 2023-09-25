56a76c1c05b11e0f94dfd454b77991700d58a262
Minervus <tnguyen411@gmail.com>
2023-09-25 09:40:18 -0700

    tidied up variables moved local variables into fields refactored code into methods

--- app/src/main/java/com/tonys/bmicalculator/MainActivity.java
+++ app/src/main/java/com/tonys/bmicalculator/MainActivity.java
@@ -14,28 +14,52 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioButton radioMale;
+    private RadioButton radioFemale;
+    private EditText viewById;
+    private EditText ageText;
+    private EditText feetText;
+    private EditText inchesText;
+    private EditText weightText;
+    private Button calculateButton;
+    private TextView resultText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
 
-        RadioButton radioMale = findViewById(R.id.radio_button_male);
-        RadioButton radioFemale = findViewById(R.id.radio_button_female);
 
-        EditText ageText = findViewById(R.id.text_view_age);
-        EditText feetText = findViewById(R.id.text_view_feet);
-        EditText inchesText = findViewById(R.id.text_view_inches);
-        EditText weightText = findViewById(R.id.text_view_weight);
+        setupButtonClickListener();
+    }
 
-        Button calculateButton = findViewById(R.id.button_calculate);
-        TextView resultText = findViewById(R.id.text_view_result);
+    private void findViews(){
+        radioMale = findViewById(R.id.radio_button_male);
+        radioFemale = findViewById(R.id.radio_button_female);
+        viewById = findViewById(R.id.text_view_age);
+        ageText = viewById;
+        feetText = findViewById(R.id.text_view_feet);
+        inchesText = findViewById(R.id.text_view_inches);
+        weightText = findViewById(R.id.text_view_weight);
+        calculateButton = findViewById(R.id.button_calculate);
+        resultText = findViewById(R.id.text_view_result);
+    }
 
+    private void setupButtonClickListener() {
         calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Autocomplete is OP", Toast.LENGTH_SHORT).show();
+                calculateBMI();
             }
         });
-
     }
+
+    private void calculateBMI() {
+    }
+
+
+
+
+
 }
